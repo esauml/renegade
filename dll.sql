276fffddec0d49c7b14a959ebcf65b4a58f2985e
coverend94 <oscarverduzco941202@gmail.com>
2022-03-30 17:09:57 -0600

    correccion doc bd

--- dll.sql
+++ dll.sql
@@ -3,118 +3,133 @@ CREATE DATABASE IF NOT EXISTS renegade;
 
 USE renegade;
 
-CREATE TABLE IF NOT EXISTS Usuario
-(
-    id           INT AUTO_INCREMENT PRIMARY KEY,
-    nombres      VARCHAR(50)  NOT NULL,
-    apellidos    VARCHAR(50)  NOT NULL,
-    correo       VARCHAR(100) NOT NULL UNIQUE,
-    password     VARCHAR(255) NOT NULL,
-    active       TINYINT(1) DEFAULT 0,
-    confirmed_at DATETIME
-);
-
-CREATE TABLE IF NOT EXISTS Rol
-(
+CREATE TABLE IF NOT EXISTS Rol (
     id          INT AUTO_INCREMENT PRIMARY KEY,
-    name        VARCHAR(50) NOT NULL,
+    name        VARCHAR(50),
     description VARCHAR(255)
 );
 
-
-CREATE TABLE IF NOT EXISTS Rol_Usuario
-(
-    idUsuario INT,
+CREATE TABLE IF NOT EXISTS Usuario (
+    id        INT AUTO_INCREMENT PRIMARY KEY,
+    nombres   VARCHAR(50),
+    apellidos VARCHAR(50),
+    correo    VARCHAR(100) UNIQUE,
+    password  VARCHAR(255),
+    active    TINYINT(1) DEFAULT 0,
     idRol     INT,
-    PRIMARY KEY (idUsuario, idRol),
-    FOREIGN KEY (idUsuario) REFERENCES Usuario (id),
     FOREIGN KEY (idRol) REFERENCES Rol (id)
 );
 
-CREATE TABLE IF NOT EXISTS Proveedor
-(
+CREATE TABLE IF NOT EXISTS Proveedor (
     id       INT AUTO_INCREMENT PRIMARY KEY,
-    nombre   VARCHAR(50)  NOT NULL, -- Nombre de la empresa
-    contacto VARCHAR(50)  NOT NULL, -- Nombre de la persona (Contacto persona)
-    telefono VARCHAR(20)  NOT NULL,
-    correo   VARCHAR(100) NOT NULL
+    nombre   VARCHAR(50), -- Nombre de la empresa
+    contacto VARCHAR(50), -- Nombre de la persona (Contacto persona)
+    telefono VARCHAR(20),
+    correo   VARCHAR(100)
 );
 
-CREATE TABLE IF NOT EXISTS MateriaPrima
-(
+CREATE TABLE IF NOT EXISTS MateriaPrima (
     id          INT AUTO_INCREMENT PRIMARY KEY,
-    nombre      VARCHAR(50) NOT NULL,
-    descripcion TEXT        NOT NULL,
-    costo       FLOAT       NOT NULL,
-    stock       INT         NOT NULL,
-    idProveedor INT         NOT NULL,
+    nombre      VARCHAR(50),
+    descripcion TEXT,
+    cantidad    INT,
+    unidad      VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS Compra (
+    id          INT AUTO_INCREMENT PRIMARY KEY,
+    folio       VARCHAR(50) UNIQUE,
+    idProveedor INT,
+    fechaCompra DATE,
     FOREIGN KEY (idProveedor) REFERENCES Proveedor (id)
 );
 
-CREATE TABLE IF NOT EXISTS Producto
-(
-    id          INT AUTO_INCREMENT PRIMARY KEY,
-    nombre      VARCHAR(50) NOT NULL,
-    descripcion TEXT        NOT NULL,
-    activo      TINYINT
+
+CREATE TABLE IF NOT EXISTS StockMateriaPrima (
+    id             INT AUTO_INCREMENT PRIMARY KEY,
+    cantidad       FLOAT,
+    idMateriaPrima INT,
+    idOrdenCompra  INT,
+    FOREIGN KEY (idMateriaPrima) REFERENCES MateriaPrima (id),
+    FOREIGN KEY (idOrdenCompra) REFERENCES Compra (id)
 );
 
-CREATE TABLE IF NOT EXISTS Estructura
-(
+CREATE TABLE IF NOT EXISTS CompraStockMateria (
+    idOrdenCompra  INT,
+    idMateriaPrima INT,
+    cantidad       INT,
+    costo          FLOAT,
+    PRIMARY KEY (idOrdenCompra, idMateriaPrima),
+    FOREIGN KEY (idMateriaPrima) REFERENCES MateriaPrima (id),
+    FOREIGN KEY (idOrdenCompra) REFERENCES Compra (id)
+);
+
+
+CREATE TABLE IF NOT EXISTS Producto (
+    id          INT AUTO_INCREMENT PRIMARY KEY,
+    nombre      VARCHAR(50),
+    descripcion TEXT,
+    precio      FLOAT,
+    talla       VARCHAR(50),
+    stock       INT DEFAULT 0,
+    image_url   TEXT,
+    activo tinyint
+);
+
+
+
+CREATE TABLE IF NOT EXISTS Estructura (
     id             INT AUTO_INCREMENT PRIMARY KEY,
-    idProducto     INT NOT NULL,
-    idMateriaPrima INT NOT NULL,
-    cantidad       INT NOT NULL,
+    idProducto     INT,
+    idMateriaPrima INT,
+    cantidad       INT,
     FOREIGN KEY (idProducto) REFERENCES Producto (id),
     FOREIGN KEY (idMateriaPrima) REFERENCES MateriaPrima (id)
 );
 
-CREATE TABLE IF NOT EXISTS Carrito
-(
+CREATE TABLE IF NOT EXISTS Carrito (
     id        INT AUTO_INCREMENT PRIMARY KEY,
     status    TINYINT(1) DEFAULT 0, -- 0 -> Activo | 1 -> Vendido
-    idUsuario INT NOT NULL,
+    idUsuario INT,
     FOREIGN KEY (idUsuario) REFERENCES Usuario (id)
 );
 
-CREATE TABLE IF NOT EXISTS ProductoCarrito
-(
-    idProducto INT NOT NULL,
-    idCarrito  INT NOT NULL,
+CREATE TABLE IF NOT EXISTS ProductoCarrito (
+    idProducto INT,
+    idCarrito  INT,
     cantidad   INT,
+    precio     FLOAT,
     PRIMARY KEY (idProducto, idCarrito),
     FOREIGN KEY (idProducto) REFERENCES Producto (id),
     FOREIGN KEY (idCarrito) REFERENCES Carrito (id)
 );
 
-CREATE TABLE IF NOT EXISTS Venta
-(
+CREATE TABLE IF NOT EXISTS Venta (
     id        INT AUTO_INCREMENT PRIMARY KEY,
-    total     TINYINT(1) DEFAULT 0,
-    fecha     DATETIME NOT NULL,
-    idCarrito INT      NOT NULL,
+    folio     VARCHAR(50) UNIQUE,
+    total     FLOAT,
+    fecha     DATETIME,
+    idCarrito INT,
     FOREIGN KEY (idCarrito) REFERENCES Carrito (id)
 );
 
-CREATE TABLE IF NOT EXISTS Log
-(
-    id             INT AUTO_INCREMENT PRIMARY KEY,
-    cantidad       INT NOT NULL,
-    idProducto     INT NOT NULL,
-    idMateriaPrima INT,
+CREATE TABLE IF NOT EXISTS MaterialUsado (
+    idProducto          INT,
+    idStockMateriaPrima INT,
+    cantidad            INT,
+    PRIMARY KEY (idProducto, idStockMateriaPrima),
     FOREIGN KEY (idProducto) REFERENCES Producto (id),
-    FOREIGN KEY (idMateriaPrima) REFERENCES MateriaPrima (id)
+    FOREIGN KEY (idStockMateriaPrima) REFERENCES StockMateriaPrima (id)
 );
 
-CREATE TABLE IF NOT EXISTS Pedido
-(
-    id           INT AUTO_INCREMENT PRIMARY KEY,
-    cantidad     INT DEFAULT 0,
-    idProducto   INT  NOT NULL,
-    idUsuario    INT  NOT NULL,
-    fechaEntrega DATE NOT NULL,
+
+-- Esta tabla es para resurtir en la aplicación los productos, el administrador gestiona la falta de productos
+CREATE TABLE IF NOT EXISTS Pedido (
+    id          INT AUTO_INCREMENT PRIMARY KEY,
+    cantidad    INT DEFAULT 0,
+    idProducto  INT,
+    idUsuario   INT,
+    fechaPedido DATE,
     FOREIGN KEY (idProducto) REFERENCES Producto (id),
     FOREIGN KEY (idUsuario) REFERENCES Usuario (id)
 );
-
-INSERT INTO Rol (name, description) VALUES ('administrador', 'Administrador'), ('administrativo', 'Administrativo'), ('Cliente', 'cliente');
