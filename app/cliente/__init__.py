1a1d4bb0e9a2df28c541c38baeb9a734602ae2dd
Cesar Martinez <cesargerardo258@gmail.com>
2022-03-30 17:25:22 -0600

    modulo actualizacion de usuario cliente

--- app/cliente/__init__.py
+++ app/cliente/__init__.py
@@ -1,5 +1,7 @@
 
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, redirect, render_template, request, flash, url_for
+from .ClienteQueries import Cliente as Query
+from ..config import USUARIO_CLIENTE
 
 cliente = Blueprint('cliente', __name__)
 
@@ -16,3 +18,31 @@ def agregar_producto_carrito_post():
     # id producto
     #cantidad
     return 0
+
+@cliente.route("/index",methods=["GET"])
+def index():
+    return render_template("cliente/index.html")
+
+@cliente.route('/mi-informacion')
+def miInformacion():
+    queries=Query()
+    try:
+        cliente=queries.consultarCliente(tipo_usuario=USUARIO_CLIENTE,id=1)
+        return render_template("cliente/infoUsuario.html",cliente=cliente[0])
+    except Exception as e:
+        raise e
+        return render_template("cliente/infoUsuario.html",expetion=e)
+    
+@cliente.route('/actualizar-cliente', methods=["POST"])
+def actualizar_Cliente():
+    queries=Query()
+    try:
+        cliente=queries.actualizarUsuario(nombre=request.form.get("nombres"),apellidos=request.form.get("apellidos"),
+                                          email=request.form.get("correo"),password=request.form.get("contraseña"),
+                                          id=request.form.get("idCliente"),tipo_usuario=USUARIO_CLIENTE)
+        return redirect(url_for('cliente.miInformacion'))
+        
+    except Exception as e:
+        raise e
+        return render_template("cliente/infoUsuario.html",expetion=e)
+    
