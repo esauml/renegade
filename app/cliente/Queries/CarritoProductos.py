6668267b4d6c6644fef871882ac9d71f01c026fd
Esau Martinez <esaumtz4722@gmail.com>
2022-03-31 10:04:06 -0600

    feat(cliente): add def of queries carrito

--- app/cliente/Queries/CarritoProductos.py
+++ app/cliente/Queries/CarritoProductos.py
@@ -2,5 +2,7 @@
 
 class QueriesCarrito():
 
-    def carrito_usuario(id_usuario):
+    def carrito_usuario(self, id_usuario):
         return 0
+    
+    def agregar_producto(self, USER_TYPE, id_user, id_producto, cantidad)
