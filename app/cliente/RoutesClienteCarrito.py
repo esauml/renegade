109eeb80f7c9ab017944e974aaf07b37c7bf54e4
Esau Martinez <esaumtz4722@gmail.com>
2022-03-30 18:45:07 -0600

    feat(cliente): routes carrito compras

--- app/cliente/RoutesClienteCarrito.py
+++ app/cliente/RoutesClienteCarrito.py
@@ -1,7 +1,21 @@
 from flask import Blueprint, render_template, request
 from ..config import USUARIO_CLIENTE as USER_TYPE
-from ..productos.ProductosQueries import Producto as Query
+from .Queries.CarritoProductos import QueriesCarrito as Query
 
 
 cliente_carrito_name = "CLIENTE_CARRITO"
 cliente_carrito_blueprint = Blueprint(cliente_carrito_name, __name__)
+
+
+@cliente_carrito_blueprint.route("/carrito-productos/<id>", methods=['GET'])
+def carrito_productos(id):
+
+    query = Query()
+    
+    try:
+        carrito_usuario = query.carrito_usuario(USER_TYPE, id)
+
+        print(carrito_usuario)
+        return render_template('cliente/carrito.html', carrito=carrito_productos)
+    except Exception as e:
+        raise e
