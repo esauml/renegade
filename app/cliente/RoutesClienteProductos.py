cadae89507c8cc0e4da8aa4da315b2f54b9c5c2e
Esau Martinez <esaumtz4722@gmail.com>
2022-03-31 10:02:02 -0600

    feat(cliente): add route agregar carrito

--- app/cliente/RoutesClienteProductos.py
+++ app/cliente/RoutesClienteProductos.py
@@ -74,26 +74,20 @@ def max_possible_stock(id):
 
 
 @cliente_productos_blueprint.route("/cliente/agregar-producto-producto", methods=['POST'])
-def aregar_producto_carrito(id):
+def aregar_producto_carrito():
     # inputs
-    producto_id = request.json
-    producto_id = 0 
-    user_id = 0
-    stock
+    cliente = request.form.get('id-cliente')
+    producto = request.form.get('id-producto') 
+    cantidad = request.form.get('cantidad')
 
 
     # init query handler
-    queries = Query()
+    queries = QueryCarrito()
     # consulta
     try:
-        producto_por_id = queries.consultar_producto_por_id(
-            USER_TYPE, producto_id)
+        queries.agregar_producto(
+            USER_TYPE, cliente, producto, cantidad)
 
-        # STOCK FOR SPECIFIC
-        # stock = queries.consultar_stock_producto(USER_TYPE, producto_id)
-        stock = 0  # default for now
-
-        print(producto_por_id)
-        return render_template('administrador/detalle-producto.html', producto=producto_por_id, stock=stock)
+        return redirect(url_for('CLIENTE_CARRITO.carrito_productos'))
     except Exception as e:
         raise e
