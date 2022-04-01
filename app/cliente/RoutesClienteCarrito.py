e10710d83f5e2fd4ede37b24865ae7797d7eb2a9
Esau Martinez <esaumtz4722@gmail.com>
2022-04-01 12:51:31 -0600

    feat(cliente): add endpoint eliminar producto de carrito

--- app/cliente/RoutesClienteCarrito.py
+++ app/cliente/RoutesClienteCarrito.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, g, render_template, request
+from flask import Blueprint, g, redirect, render_template, request, url_for
 from ..config import USUARIO_CLIENTE as USER_TYPE
 from .Queries.CarritoProductos import QueriesCarrito as Query
 
@@ -21,3 +21,19 @@ def carrito_productos():
         return render_template('/cliente/micarrito.html', carrito=carrito_usuario)
     except Exception as e:
         raise e
+
+@cliente_carrito_blueprint.route("/cliente/eliminar-producto-carrito", methods=['GET'])
+def carrito_productos():
+
+    carrito = request.form.get('id_carrito')
+    producto = request.form.get('id_producto')
+
+    query = Query()
+
+    try:
+        query.eliminar_producto_de_carrito(USER_TYPE, carrito, producto)
+        
+        return redirect(url_for('cliente.CLIENTE_CARRITO.carrito_productos'))
+    except Exception as e:
+        raise e
+
