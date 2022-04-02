e07d4ca5f5f78cd7f581bb2035ca5a74fc67d45c
Esau Martinez <esaumtz4722@gmail.com>
2022-04-01 23:14:12 -0600

    feat(finanzas): query ganancia mes actual

--- app/administrativo/Queries/FinanzasQuery.py
+++ app/administrativo/Queries/FinanzasQuery.py
@@ -1,5 +1,5 @@
 from ...bd import obtener_conexion
-from ...config import USUARIO_ADMINISTATIVO
+from ...config import USUARIO_ADMINISTATIVO as USER_TYPE
 
 
 class QueriesFinanzas():
@@ -12,7 +12,25 @@ class QueriesFinanzas():
             raise Exception(e)
     
     def ganancia_mes_actual():
-        return 0
+        try:
+            query = '\
+                select ifnull(sum(precio), 0) from ProductoCarrito where idCarrito in (\
+                    select id from carrito where id in (\
+                        select idCarrito from Venta where month(fecha) = month(NOW()) and year(fecha) = year(now())\
+                    )\
+                );'
+            conexion = obtener_conexion(USER_TYPE)
+            ganancia_anual = 0
+
+            with conexion.cursor() as cursor:
+                cursor.execute(query, (id,))
+                ganancia_anual = cursor.fetchall()
+
+            cursor.close()
+            return ganancia_anual
+            
+        except Exception as e:
+            raise Exception(e)
 
     def ganancia_meses_anio():
         return 0
