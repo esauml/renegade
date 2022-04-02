291251e0c5e6f32c92c28e63d9bef5c7b47fe3b3
Esau Martinez <esaumtz4722@gmail.com>
2022-04-01 23:51:55 -0600

    feat(finanzas): add endpoint logic

--- app/administrativo/__init__.py
+++ app/administrativo/__init__.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, g, session, flash, render_template, redirect, request, url_for
 from .Queries.productoQuery import Producto
+from .Queries.FinanzasQuery import QueriesFinanzas
 from ..site import UsuarioQueries
 from ..config import USUARIO_ADMINISTATIVO
 from .RoutesAdminProducto import producto
@@ -24,7 +25,14 @@ def before_request_administrativo():
 
 @administrativo.route("/consultar-ventas", methods=['GET'])
 def consultar_ventas_get():
-    return render_template('adm/index.html')
+    query = QueriesFinanzas()
+    
+    monthly = query.ganancia_mes_actual()
+    yearly = query.ganancia_anual()
+    monthes_earnings = query.ganancia_meses_anio()
+    top10 = query.top_10_productos_vendidos()
+    
+    return render_template('adm/index.html', monthly = monthly, yearly = yearly, monthes_earnings = monthes_earnings, top10 = top10)
 
 
 @administrativo.route("/consultar-rendimiento", methods=['GET'])
