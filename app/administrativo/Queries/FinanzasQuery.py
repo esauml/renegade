153200be4eac63b8f39c6fc533eace9780ea4943
Esau Martinez <esaumtz4722@gmail.com>
2022-04-01 22:26:11 -0600

    feat(cliente): define Finanzas Query

---
+++ app/administrativo/Queries/FinanzasQuery.py
@@ -0,0 +1,21 @@
+from ...bd import obtener_conexion
+from ...config import USUARIO_ADMINISTATIVO
+
+
+class QueriesFinanzas():
+    
+    def ganancia_anual():
+        try:
+            return 0
+            
+        except Exception as e:
+            raise Exception(e)
+    
+    def ganancia_mes_actual():
+        return 0
+
+    def ganancia_meses_anio():
+        return 0
+    
+    def top_10_productos_vendidos():
+        return 0    
