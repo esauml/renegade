b2014db4d6e9b5e0bbee8d668404cdefd422eda2
Esau Martinez <esaumtz4722@gmail.com>
2022-04-01 23:35:54 -0600

    feat(finanzas): query ganancia meses anio

--- app/administrativo/Queries/FinanzasQuery.py
+++ app/administrativo/Queries/FinanzasQuery.py
@@ -47,7 +47,27 @@ class QueriesFinanzas():
             raise Exception(e)
 
     def ganancia_meses_anio():
-        return 0
+        try:
+            query = '\
+                select Month(fecha) index_meses, \
+                    (select ifnull(sum(precio), 0) from ProductoCarrito where idCarrito in (\
+                        select id from carrito where id in (\
+                            select idCarrito from Venta where month(fecha) = index_meses and year(fecha) = year(now())\
+                        )\
+                    )) ventas\
+                from Venta where year(fecha) = year(now()) group by index_meses;'
+            conexion = obtener_conexion(USER_TYPE)
+            ganancia_anual = 0
+
+            with conexion.cursor() as cursor:
+                cursor.execute(query, (id,))
+                ganancia_anual = cursor.fetchall()
+
+            cursor.close()
+            return ganancia_anual
+            
+        except Exception as e:
+            raise Exception(e)
     
     def top_10_productos_vendidos():
         return 0    
